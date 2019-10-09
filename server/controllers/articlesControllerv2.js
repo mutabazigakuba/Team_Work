4b277b182fa2c0d4ef7b4fb5c9454e3b856d67ed
mutabazigakuba <gakubar2@gmail.com>
2019-10-09 13:43:38 +0200

    [Finishes #168981055] added and connected the delete article from database

--- server/controllers/articlesControllerv2.js
+++ server/controllers/articlesControllerv2.js
@@ -27,6 +27,31 @@ const ArticleControllerv2 = {
             })
         }
     },
+    async deletedArticle(req, res) {
+        const findarticle = 'SELECT * FROM articles WHERE id=$1';
+        try {
+            const { rows } = await db.query(findarticle, [req.params.articleid]);
+            if (!rows[0]) {
+                return res.status(404).send({
+                    "status": 404,
+                    "error": "article not found"
+                });
+            }
+            const deleteQuery = 'DELETE FROM articles WHERE id=$1 returning *';
+            const { row } = await db.query(deleteQuery, [req.params.articleid]);
+            console.log(row[0])
+            return res.status(204).send({
+                "status": 204,
+                "message": "article successfully deleted"
+            });
+        } catch (e) {
+            console.log(e)
+            return res.status(500).send({
+                "status": 500,
+                "error": "server error"
+            })
+        }
+    },
 }
 
 export default ArticleControllerv2;
--- server/routes/routesv2.js
+++ server/routes/routesv2.js
@@ -12,5 +12,6 @@ routesv2.get('/', function (req, res) {return res.send('Hello TeamWork');});
 routesv2.post('/api/v2/auth/signup', userValidator , User.createNewuser);
 routesv2.post('/api/v2/auth/signin', loginValidator, User.login );
 routesv2.post('/api/v2/articles', auth, ArticleValidator.createArticles, Article.createArticle)
+routesv2.delete('/api/v2/articles/:articleid',auth, Article.deletedArticle);
 
 export default routesv2;
