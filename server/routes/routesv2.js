6f50df118a07269eddaee75fea556e4b01e95dfa
mutabazigakuba <gakubar2@gmail.com>
2019-10-09 10:01:59 +0200

    [Finishes #168981007] connected the login endpoint to the database

--- server/routes/routesv2.js
+++ server/routes/routesv2.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import userValidator from '../middleware/userValidator.js';
 import User from '../controllers/userControllerv2';
+import loginValidator from '../middleware/loginValidator';
 
 const routesv2 = express.Router();
 
 routesv2.get('/', function (req, res) {return res.send('Hello TeamWork');});
 routesv2.post('/api/v2/auth/signup', userValidator , User.createNewuser);
+routesv2.post('/api/v2/auth/signin', loginValidator, User.login );
 
 export default routesv2;
